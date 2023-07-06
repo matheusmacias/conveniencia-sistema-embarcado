9ca8e5fa1a25a50bd6cee63ebd0d63441eaeaf64
matheusmacias <matheusmacias3@gmail.com>
2023-07-05 21:40:29 -0300

    update main

--- main/main.ino
+++ main/main.ino
@@ -12,12 +12,12 @@ CustomJWT jwt(key, 256);
 
 /* ----------- CONFIG SERVIDOR ------------ */
 byte mac[] = { 0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02 };
-IPAddress ip(10, 10, 81, 215);  // Endereço IP que a Ethernet Shield terá. Deve ser alterado para um endereço livre da sua rede.
+IPAddress ip(10, 10, 80, 220);  // Endereço IP que a Ethernet Shield terá. Deve ser alterado para um endereço livre da sua rede.
 int port(80);                   // Cria um servidor WEB
 
 WebServer webServer(mac, ip, port);
 
-/* ----------------- JSON ----------------- */
+/* ----------------- JSON ----------------- *///http://10.10.80.224:3000/
 StaticJsonDocument<200> doc;
 
 /* ---------- PROTÓTIPOS FUNÇÕES ---------- */
@@ -27,6 +27,8 @@ void setup() {
   Serial.begin(115200);
 
   webServer.begin();
+  pinMode(9, OUTPUT);
+
 
   Serial.println("Servidor está sendo iniciado.");
 }
@@ -49,10 +51,13 @@ void handleHeaders(){
       //   Serial.print(F("deserializeJson() failed: "));
       //   Serial.println(error.f_str());
       // }
-      const char* name = doc["name"];
+      const char* name = doc["_email"];
       if(name){
         Serial.println("Abrir Porta!");
         webServer.sendHeaders_OK();
+        digitalWrite(9, HIGH);
+        delay(3000);
+        digitalWrite(9, LOW);
       }else{
         Serial.println("Acesso Negado!");
         webServer.sendHeaders_Forbidden();
